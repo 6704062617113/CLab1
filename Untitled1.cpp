5d33d11c19d9c356dcc09c830f52957e44d9c903
6704062617113 <s6704062617113@email.kmutnb.ac.th>
2024-11-28 18:26:41 +0700

    Add files via upload

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,11 +1,10 @@
 #include <stdio.h>
 int main() {
-	int num1=10;
-	int num2=5;
-	if (num1>num2){
+	int num1 = 10;
+	int num2 = 50;
+	if (num1 > num2){
 		printf("num1 is greater then num2");
-	}
-	else{
+	} else {
 		printf("num2 is greater then num1");
 	}
 }
