4568637a23251eef09983016f63c617f0f3ba0d8
6704062617113 <s6704062617113@email.kmutnb.ac.th>
2024-12-10 15:45:50 +0700

    Add files via upload

--- lab1minx.c
+++ lab1minx.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 int main() {
-	int num1=10;
-	int num2=50;
+	int num1,num2;
+	printf("n1=");
+	scanf("%d",&num1);
+	printf("n2=");
+	scanf("%d",&num2);
 	if (num1>num2){
-		printf("num1 is greater then num2");
+		printf("num1 is more then num2 %d\n",num1-num2);
 	}
 	else{
-		printf("num2 is greater then num1");
+		printf("num2 is more then num1 %d\n",num2-num1);
 	}
 }
+
